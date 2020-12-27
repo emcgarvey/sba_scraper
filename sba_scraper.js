007d70d1fad16a944904da9b5cd282866018b93a
Ethan McGarvey <ejm5413@gmail.com>
2020-12-26 21:25:12 -0700

    successfully logs in and then proceeds to log out. json for username/password next and then handling multiple sessions

--- sba_scraper.js
+++ sba_scraper.js
@@ -1,9 +1,25 @@
 const puppeteer = require('puppeteer');
-
-async function scrapeMLS(url) {
+const fs = require('fs');
+const cookies = require('./cookies.json');
+async function scrapeMLS(url) 
+{
+    /*Start puppeteer and create a new page*/
     const browser = await puppeteer.launch({headless:false});
     const page = await browser.newPage();
-    await page.goto(url)
+    
+        /*go to paragon login page*/
+        await page.goto(url), {waitUntil: 'networkidle0'};
+        /*enter username and password*/
+        await page.type('#LoginName', 'username', {delay: 30});
+        await page.type('#Password', 'password', {delay: 30});
+
+        /* Click the login button*/
+        await page.click('#Enter')
+        //await page.
+        await page.waitForNavigation();
+        //await page.waitFor(3000);
+        await page.click('#user_menu_name');
+        await page.click('#logoff');
 }
 
 scrapeMLS('https://cheyenne.paragonrels.com/ParagonLS/Default.mvc/Login');
